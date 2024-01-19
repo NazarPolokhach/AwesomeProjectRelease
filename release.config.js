12b45398e8d42c159522007acacbba29eb3ddef4
Nazar <npolohach@gmail.com>
2024-01-19 16:32:39 +0200

    Add release

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  branches: ['master'],
+  npmPublish: false,
+  // We're initiating the release from
+  // our local machine.
+  ci: false,
+  plugins: [
+    // Analyzes commit messages and
+    // determines the version
+    '@semantic-release/commit-analyzer',
+    // Builds release notes from commit messages
+    '@semantic-release/release-notes-generator',
+    // Updates the version
+    '@semantic-release/npm',
+    [
+      // Commits, tags, and pushes
+      '@semantic-release/git',
+      {
+        assets: [
+          'ios/donesafe*/Info.plist',
+          'ios/donesafe.xcodeproj/project.pbxproj',
+          'android/app/build.gradle',
+          'package.json',
+        ],
+        message:
+          'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+      },
+    ],
+    // Creates the release on GitHub
+    // and posts release notes
+    '@semantic-release/github',
+  ],
+};
